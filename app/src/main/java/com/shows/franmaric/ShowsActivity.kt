b265c1de67addfa4cbf707b954905dd7a2f36210
Fran Marić <fran.maric.vk@gmail.com>
2021-07-15 11:21:52 +0200

    Working demo with ShowsActivity

--- app/src/main/java/com/shows/franmaric/ShowsActivity.kt
+++ app/src/main/java/com/shows/franmaric/ShowsActivity.kt
@@ -1,4 +1,15 @@
 package com.shows.franmaric
 
-class ShowsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shows.franmaric.databinding.ActivityShowsBinding
+
+class ShowsActivity : AppCompatActivity()  {
+    private lateinit var binding: ActivityShowsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityShowsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 }
