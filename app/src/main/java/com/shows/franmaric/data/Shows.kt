fd02e7708d815ae1090c65a3aa8796ec7afc96de
Fran Marić <fran.maric.vk@gmail.com>
2021-07-16 00:10:09 +0200

    Modified data model to match mentor instructions from slack

--- app/src/main/java/com/shows/franmaric/data/Shows.kt
+++ app/src/main/java/com/shows/franmaric/data/Shows.kt
@@ -2,9 +2,12 @@ package com.shows.franmaric.data
 
 import com.shows.franmaric.models.Show
 
-val shows = listOf(
-    Show("1","Krv nije voda ep1","Leon voli sve što vole mladi.","knv1"),
-    Show("2","Krv nije voda ep2","Prosječni ferovac.","knv2"),
-    Show("3","Krv nije voda ep3","Roditelji samo žele svoju kćerku bogato udati.","knv3"),
-    Show("4","Krv nije voda ep4","Anđela je izgubila povjerenje u Luku","knv4")
-)
+
+object ShowsResources{
+    val shows = listOf(
+        Show("1","Krv nije voda ep1","Leon voli sve što vole mladi.","knv1"),
+        Show("2","Krv nije voda ep2","Prosječni ferovac.","knv2"),
+        Show("3","Krv nije voda ep3","Roditelji samo žele svoju kćerku bogato udati.","knv3"),
+        Show("4","Krv nije voda ep4","Anđela je izgubila povjerenje u Luku.","knv4")
+    )
+}
