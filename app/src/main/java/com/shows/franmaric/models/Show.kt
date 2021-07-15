45f9381c9a9688051e6f085d45fa235438b9895e
Fran Marić <fran.maric.vk@gmail.com>
2021-07-16 00:17:16 +0200

    Converted last argument type of Show from String to Int (local file)

--- app/src/main/java/com/shows/franmaric/models/Show.kt
+++ app/src/main/java/com/shows/franmaric/models/Show.kt
@@ -1,5 +1,5 @@
 package com.shows.franmaric.models
 
-data class Show (val ID:String,val name:String,val description:String,val imgSrc:String){
+data class Show (val ID:String,val name:String,val description:String,val imgSrc:Int){
 
 }
