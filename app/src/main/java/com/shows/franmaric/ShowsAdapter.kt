6dc9ba6aa46a2835fd103fbd0de3141a56819b2a
Fran Marić <fran.maric.vk@gmail.com>
2021-07-16 22:42:29 +0200

    ShowsAdapter completed

--- app/src/main/java/com/shows/franmaric/ShowsAdapter.kt
+++ app/src/main/java/com/shows/franmaric/ShowsAdapter.kt
@@ -1,4 +1,52 @@
 package com.shows.franmaric
 
-class ShowsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shows.franmaric.databinding.ViewShowItemBinding
+import com.shows.franmaric.models.Show
+
+class ShowsAdapter(
+    private var items: List<Show>,
+    private val onItemClickCallback: (Show) -> Unit
+)  : RecyclerView.Adapter<ShowsAdapter.ShowViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShowViewHolder {
+        val binding = ViewShowItemBinding.inflate(LayoutInflater.from(parent.context))
+
+        return ShowViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: ShowViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    fun setItems(shows: List<Show>) {
+        items = shows
+        notifyDataSetChanged()
+    }
+
+    fun addItem(show: Show) {
+        items = items + show
+        notifyItemInserted(items.size)
+    }
+
+    inner class ShowViewHolder(private val binding: ViewShowItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Show) {
+            binding.showImage.setImageResource(item.imageResourceId)
+
+            binding.titleTextView.text = item.name
+
+            binding.descriptionTextView.text = item.description
+
+            binding.root.setOnClickListener {
+                onItemClickCallback(item)
+            }
+        }
+    }
 }
