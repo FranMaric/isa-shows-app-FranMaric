53a1cc255613e4ae03f88cd64474c3044caab56b
Fran Marić <fran.maric.vk@gmail.com>
2021-07-19 15:46:17 +0200

    In progress

--- app/src/main/java/com/shows/franmaric/ShowDetailsActivity.kt
+++ app/src/main/java/com/shows/franmaric/ShowDetailsActivity.kt
@@ -2,10 +2,19 @@ package com.shows.franmaric
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.shows.franmaric.databinding.ActivityLoginBinding
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.shows.franmaric.data.ShowsResources
 import com.shows.franmaric.databinding.ActivityShowDetailsBinding
+import com.shows.franmaric.databinding.DialogAddReviewBinding
+import com.shows.franmaric.models.Review
+import com.shows.franmaric.models.Show
+
 
 class ShowDetailsActivity : AppCompatActivity() {
     companion object {
@@ -20,12 +29,110 @@ class ShowDetailsActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityShowDetailsBinding
+    private var reviewsAdapter: ReviewsAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityShowDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val show = ShowsResources.shows[intent.getIntExtra(SHOW_INDEX,0)]
+
+        setSupportActionBar(binding.toolbar)
+
+        initDataContainers(show)
+
+        initReviewRecycler()
+
+        initSubmitButton()
+    }
+
+    private fun initDataContainers(show: Show) {
+        binding.collapsingToolbarLayout.title = show.name
+        binding.toolbar.setNavigationOnClickListener {
+            println("IDEMOOOO NAZAD")
+        }
+        binding.collapsingToolbarLayout.setExpandedTitleColor(Color.BLACK)
+        binding.collapsingToolbarLayout.setCollapsedTitleTextColor(Color.BLACK)
+        binding.showImageView.setImageResource(show.imageResourceId)
+        binding.descriptionTextView.text = show.description
+    }
+
+    private fun initSubmitButton() {
+        binding.reviewButton.setOnClickListener{
+            showBottomSheet()
+        }
+    }
+
+    private fun showBottomSheet() {
+        val dialog = BottomSheetDialog(this)
+
+        val bottomSheetBinding = DialogAddReviewBinding.inflate(layoutInflater)
+        dialog.setContentView(bottomSheetBinding.root)
+
+        bottomSheetBinding.dismissButton.setOnClickListener{
+            dialog.dismiss()
+        }
+
+        bottomSheetBinding.submitButton.setOnClickListener {
+            val rating = bottomSheetBinding.ratingBar.rating.toInt()
+            val comment = bottomSheetBinding.commentField.text.toString()
+
+            val review = Review(rating, comment)
+            reviewsAdapter?.addItem(review)
+
+            var newRating = 0.0
+            reviewsAdapter?.getItems()?.forEach{review->
+                newRating += review.rating
+            }
+
+            var reviewCount = reviewsAdapter?.getItemCount()!!
+            newRating = newRating / reviewCount
+
+            binding.reviewInfoTextView.text = "${reviewCount} REVIEW${if(reviewCount == 1) "" else "S"}, ${newRating} AVERAGE"
+
+            binding.ratingBar.rating = newRating.toFloat()
+
+            setRecyclerViewVisibility(true)
+
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun setRecyclerViewVisibility(visible:Boolean){
+        // Saving processing power :)
+        if(binding.reviewsRecyclerView.isVisible == visible)
+            return
+
+        if(visible){
+            binding.reviewsRecyclerView.isVisible = true
+            binding.reviewInfoTextView.isVisible = true
+            binding.ratingBar.isVisible = true
+            binding.emptyStateLabel.isVisible = false
+        }
+        else {
+            binding.reviewsRecyclerView.isVisible = false
+            binding.reviewInfoTextView.isVisible = false
+            binding.ratingBar.isVisible = false
+            binding.emptyStateLabel.isVisible = true
+        }
+    }
+
+    private fun initReviewRecycler() {
+        reviewsAdapter = ReviewsAdapter(emptyList())
+
+        if(reviewsAdapter?.getItemCount() == 0){
+            setRecyclerViewVisibility(false)
+        } else {
+            setRecyclerViewVisibility(true)
+        }
+
+        binding.reviewsRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.reviewsRecyclerView.adapter = reviewsAdapter
+
+        binding.reviewsRecyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 
 }
