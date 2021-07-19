008593750b492db8c78860ada17f98c3d3ae90a1
Fran Marić <fran.maric.vk@gmail.com>
2021-07-19 15:45:40 +0200

    Added ReviewsAdapter

---
+++ app/src/main/java/com/shows/franmaric/ReviewsAdapter.kt
@@ -0,0 +1,49 @@
+package com.shows.franmaric
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.shows.franmaric.databinding.ViewReviewItemBinding
+import com.shows.franmaric.models.Review
+
+class ReviewsAdapter(
+    private var items: List<Review>
+)  : RecyclerView.Adapter<ReviewsAdapter.ReviewViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        val binding = ViewReviewItemBinding.inflate(LayoutInflater.from(parent.context))
+
+        return ReviewViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun getItems() : List<Review>{
+        return items
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    fun setItems(reviews: List<Review>) {
+        items = reviews
+        notifyDataSetChanged()
+    }
+
+    fun addItem(review: Review) {
+        items = items + review
+        notifyItemInserted(items.size)
+    }
+
+    inner class ReviewViewHolder(private val binding: ViewReviewItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(review: Review) {
+            binding.commentTextView.text = review.comment
+            binding.ratingTextView.text = review.rating.toString()
+            println("BOK izradujem viewholder. Nema na cemu.")
+        }
+    }
+}
