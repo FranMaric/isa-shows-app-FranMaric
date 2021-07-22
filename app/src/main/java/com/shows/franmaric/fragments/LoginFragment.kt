118d9fa96eca78d9e62483a98a2bbc1c7f06550a
Fran Marić <fran.maric.vk@gmail.com>
2021-07-22 08:49:20 +0200

    Add logic for writing remember me to prefs and redirecting to ShowFragment

--- app/src/main/java/com/shows/franmaric/fragments/LoginFragment.kt
+++ app/src/main/java/com/shows/franmaric/fragments/LoginFragment.kt
@@ -33,49 +33,66 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        checkRememberMe()
+
         initLoginButton()
 
         initInputs()
     }
 
-    private fun initLoginButton(){
+    private fun checkRememberMe() {
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        if (sharedPref.getBoolean(getString(R.string.prefs_remember_me), false)) {
+            val action = LoginFragmentDirections.actionLoginToShows()
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun initLoginButton() {
         binding.loginButton.setEnabled(false)
-        binding.loginButton.setOnClickListener{
+        binding.loginButton.setOnClickListener {
             login()
         }
     }
 
-    private fun initInputs(){
-        binding.emailField.doAfterTextChanged { email->
+    private fun initInputs() {
+        binding.emailField.doAfterTextChanged { email ->
             val password = binding.passwordField.text.toString()
-            if(isValidInput(email.toString(), password)){
+            if (isValidInput(email.toString(), password)) {
                 setButtonEnabled(true)
-            } else if(binding.loginButton.isEnabled){
+            } else if (binding.loginButton.isEnabled) {
                 setButtonEnabled(false)
             }
         }
-        binding.passwordField.doAfterTextChanged { password->
+        binding.passwordField.doAfterTextChanged { password ->
             val email = binding.emailField.text.toString()
-            if(isValidInput(email, password.toString())){
+            if (isValidInput(email, password.toString())) {
                 setButtonEnabled(true)
-            } else if(binding.loginButton.isEnabled){
+            } else if (binding.loginButton.isEnabled) {
                 setButtonEnabled(false)
             }
         }
     }
 
-    private fun setButtonEnabled(enabled:Boolean){
+    private fun setButtonEnabled(enabled: Boolean) {
         binding.loginButton.apply {
             setEnabled(enabled)
-            if(enabled){
-                setBackgroundTintList(ContextCompat.getColorStateList(requireContext(),
-                    R.color.button_enabled
-                ))
+            if (enabled) {
+                setBackgroundTintList(
+                    ContextCompat.getColorStateList(
+                        requireContext(),
+                        R.color.button_enabled
+                    )
+                )
                 setTextColor(Color.parseColor("#52368C"))
             } else {
-                setBackgroundTintList(ContextCompat.getColorStateList(requireContext(),
-                    R.color.button_disabled
-                ))
+                setBackgroundTintList(
+                    ContextCompat.getColorStateList(
+                        requireContext(),
+                        R.color.button_disabled
+                    )
+                )
                 setTextColor(Color.WHITE)
             }
         }
@@ -83,8 +100,9 @@ class LoginFragment : Fragment() {
 
     private fun login() {
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
-        with (sharedPref.edit()) {
+        with(sharedPref.edit()) {
             putString(getString(R.string.prefs_email), binding.emailField.text.toString())
+            putBoolean(getString(R.string.prefs_remember_me), binding.rememberMeCheckBox.isChecked)
             apply()
         }
 
@@ -93,7 +111,7 @@ class LoginFragment : Fragment() {
     }
 
     private fun isValidInput(email: String?, password: String?) =
-         email!=null && password!=null && isEmailValid(email) && password.length >= MIN_PASSWORD_LENGTH
+        email != null && password != null && isEmailValid(email) && password.length >= MIN_PASSWORD_LENGTH
 
     //not gonna lie I found it on stackoverflow
     private fun isEmailValid(email: String): Boolean {
