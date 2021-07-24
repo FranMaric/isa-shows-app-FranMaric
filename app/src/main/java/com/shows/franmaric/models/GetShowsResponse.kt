1ecaadcc340c64aea33fa95b1ee022c1d08a08dc
Fran Marić <fran.maric.vk@gmail.com>
2021-07-24 23:10:12 +0200

    Add new models for ShowResponse

---
+++ app/src/main/java/com/shows/franmaric/models/GetShowsResponse.kt
@@ -0,0 +1,9 @@
+package com.shows.franmaric.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetShowsResponse (
+    @SerialName("shows") val shows: List<ShowResponse>
+)
