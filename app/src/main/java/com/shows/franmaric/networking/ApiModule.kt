2e6218f34c6d12359ad1bccb22b7d043291dd26b
Fran Marić <fran.maric.vk@gmail.com>
2021-07-24 22:13:47 +0200

    Add headers for access-token and token-type into http interceptor

--- app/src/main/java/com/shows/franmaric/networking/ApiModule.kt
+++ app/src/main/java/com/shows/franmaric/networking/ApiModule.kt
@@ -3,6 +3,7 @@ package com.shows.franmaric.networking
 import android.content.SharedPreferences
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -18,7 +19,14 @@ object ApiModule {
             .addInterceptor(HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             })
-            .build()
+            .addInterceptor { chain ->
+                val requestBuilder = chain.request().newBuilder()
+                    .header("access-token", preferences.getString("access-token","").toString())
+                    .header("token-type","Bearer")
+
+                val request = requestBuilder.build()
+                chain.proceed(request)
+            }.build()
 
         retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
