b7904c057fe8224bebaddcb1bfe5d8431c02bb9e
Fran Marić <fran.maric.vk@gmail.com>
2021-07-24 21:34:10 +0200

    Revert "Add initiliza"
    This reverts commit 8e6d8c3a3d8e2f63b1d994fa027ec135cdd4ca36.

--- app/src/main/java/com/shows/franmaric/MainActivity.kt
+++ app/src/main/java/com/shows/franmaric/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.shows.franmaric
 
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.shows.franmaric.databinding.ActivityMainBinding
-import com.shows.franmaric.networking.ApiModule
 
 class MainActivity: AppCompatActivity()  {
     private lateinit var binding: ActivityMainBinding
@@ -15,7 +13,5 @@ class MainActivity: AppCompatActivity()  {
         setContentView(binding.root)
 
         supportActionBar?.hide()
-
-        ApiModule.initRetrofit(getPreferences(Context.MODE_PRIVATE))
     }
 }
