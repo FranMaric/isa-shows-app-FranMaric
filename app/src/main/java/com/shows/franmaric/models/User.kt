3e706ceee3e9394ec5a0beb851dccbd24053c404
Fran Marić <fran.maric.vk@gmail.com>
2021-07-26 20:34:21 +0200

    Switched id form Int to String in User data class

--- app/src/main/java/com/shows/franmaric/models/User.kt
+++ app/src/main/java/com/shows/franmaric/models/User.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class User(
-    @SerialName("id") val id: Int,
+    @SerialName("id") val id: String,
     @SerialName("email") val email: String,
     @SerialName("image_url") val imageUrl: String?
 )
