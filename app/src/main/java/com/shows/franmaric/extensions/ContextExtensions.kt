24792061102c7374f0cd44ae6f1578319a5d398b
Fran Marić <fran.maric.vk@gmail.com>
2021-08-05 18:36:45 +0200

    Updated extension no longer using deprecated code for newer apis

--- app/src/main/java/com/shows/franmaric/extensions/ContextExtensions.kt
+++ app/src/main/java/com/shows/franmaric/extensions/ContextExtensions.kt
@@ -1,11 +1,27 @@
 package com.shows.franmaric.extensions
 
 import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
+import android.net.NetworkCapabilities
+import android.os.Build
 
 fun Context.hasInternetConnection(): Boolean {
-    val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-    return activeNetwork?.isConnectedOrConnecting == true
+    val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+        return cm.activeNetworkInfo != null
+
+    } else {
+        for (network in cm.allNetworks) { // added in API 21 (Lollipop)
+            val networkCapabilities: NetworkCapabilities? =
+                cm.getNetworkCapabilities(network)
+            return (networkCapabilities!!.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
+                    (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                            || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                            || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
+        }
+    }
+    return false
 }
